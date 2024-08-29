e3a4a7377ee97eab9257169d6fa22148c195a7ab
Arisforprogramming <codegarden23@gmail.com>
2024-08-29 16:23:38 -0500

    need to add rest of the file

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,35 @@
+import React from 'react';
+import styling from "./styling.css";
+//import satData from "./satData"; //import info from satData
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th> Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+      {/* Table Body - Iterate through sat prop using map */}
+      {sat.map((data, id) => (
+     <tr key ={id}>
+       {/* Table Data */}
+       <td>{data.name}</td>
+       <td>{data.type}</td>
+       <td>{data.launchDate}</td> 
+       <td>{data.operational ? 'Active' : 'Inactive'}</td>  {/*using function true if active false if inactive*/}
+     </tr> 
+       ))}
      </tbody>
    </table>
   );
-};
+}; 
+
+
+
 
 export default Table;
