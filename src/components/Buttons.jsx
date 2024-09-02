16964ceea7874dc9ef33f67f649fed3691db93cc
Arisforprogramming <codegarden23@gmail.com>
2024-09-02 10:49:54 -0500

    fixed filltered table

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,14 +1,14 @@
 import satData from "./satData"; //import info from satData
-import styling from "./styling.css";
+import Styling from "./styling.css";
 
 //This function is update the buttons components 
-// there is a fuction that takes parameters
+// there is a Buttons fuction that takes parameters
 // filtersByTypes = represent  A function that filters satellites based on the orbit type in SatData files
 // setSat: A function that updates the state of the displayed satellites.
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div className="flex-container">
+    <div className="flex-container"> 
       {/* Iterate over displaySats using map 
       get array that we wanting to map over displaySats chaning with .map
       iterates over the displaySats array, where sat represents the current orbit type, and id is its index.
@@ -18,14 +18,13 @@ const Buttons = ({ filterByType, setSat, displaySats }) => {
         //it calls the filterByType function with the current orbit type (sat), filtering the displayed satellites accordingly.
         return (                //from app.jsx
           <button onClick={() => filterByType(sat)} key={id}>  {/*key={id}: The key prop, set to the index id, helps React efficiently manage the list of buttons.*/}
-            {sat} Orbit   { /*Replace "Placeholder Button" with the orbit type 
-            Button Label: The text inside the button displays the orbit type followed by "Orbit" (e.g., "LEO Orbit").*/}
+            {sat} Orbit  
           </button>
         );
       })}
       {/* Button to display all orbits 
       be outside of map function*/}
-      <button onClick={() => setSat(satData)}>All Orbits</button>
+      <button onClick={()=> setSat(satData)}>All Orbits</button>
     </div>
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
+import React from "react";
 import styling from "./styling.css";
-//import satData from "./satData"; //import info from satData
+import satData from "./satData"; //import info from satData
 
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Name</th>
-       <th>Type of Satellite</th>
-       <th>Launch Date</th>
-       <th> Status</th>
-     </tr>
-     </thead>
-     <tbody>
-      {/* Table Body - Iterate through sat prop using map */}
-      {sat.map((data, id) => (
-     <tr key ={id}>
-       {/* Table Data */}
-       <td>{data.name}</td>
-       <td>{data.type}</td>
-       <td>{data.launchDate}</td> 
-       <td>{data.operational ? 'Active' : 'Inactive'}</td>  {/*using function true if active false if inactive*/}
-     </tr> 
-       ))}
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th> Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Table Body - Iterate through sat prop using map */}
+        {sat.map((data, id) => (
+          <tr key={id}>
+            {/* Table Data */}
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational? "Active" : "Inactive"}</td>
+            {/*using function true if active false if inactive*/}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
-}; 
-
-
-
+};
 
 export default Table;
